4f65ae01d212002f5d7d66b79b47c14dc892e08b
Karachentsev Dmitry <xmitya@gmail.com>
2016-12-25 15:31:16 +0300

    Logging dependency

--- src/main/java/ru/ifmo/HelloWorld.java
+++ src/main/java/ru/ifmo/HelloWorld.java
@@ -1,10 +1,18 @@
 package ru.ifmo;
 
+import org.apache.log4j.Logger;
+
 /**
  * Created by xmitya on 25.12.16.
  */
 public class HelloWorld {
+    private static final Logger LOG = Logger.getLogger(HelloWorld.class);
+
     public static void main(String[] args) {
+        LOG.info("Application started");
+
         System.out.println("Hello, Maven!");
+
+        LOG.info("Application stopped");
     }
 }
