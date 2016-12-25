398a9a94a8aa02d67f009e63111c497fdce5afbd
Karachentsev Dmitry <xmitya@gmail.com>
2016-12-25 15:27:17 +0300

    First maven project

---
+++ src/main/java/ru/ifmo/HelloWorld.java
@@ -0,0 +1,10 @@
+package ru.ifmo;
+
+/**
+ * Created by xmitya on 25.12.16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+}
